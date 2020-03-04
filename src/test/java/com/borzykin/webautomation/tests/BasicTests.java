bf0f2f56b7ea991e6b001148aada28de936f0470
Oleksii Borzykin <borzykin3455@gmail.com>
2020-03-04 21:57:42 +0200

    Basic pom.xml setup for web automation

--- src/test/java/com/borzykin/webautomation/tests/BasicTests.java
+++ src/test/java/com/borzykin/webautomation/tests/BasicTests.java
@@ -1,4 +1,18 @@
 package com.borzykin.webautomation.tests;
 
+import lombok.extern.log4j.Log4j;
+import org.assertj.core.data.Percentage;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+@Log4j
 public class BasicTests {
+    @Test
+    public void testingTests() {
+        log.info("T E S T");
+        assertThat(2 + 2)
+                .as("Sum should be close")
+                .isCloseTo(5, Percentage.withPercentage(25));
+    }
 }
